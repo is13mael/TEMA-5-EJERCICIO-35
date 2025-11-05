888f9baa64f6c31365654b7a353012e0ced6c1ca
is13mael <ismaelparramoussaif@gmail.com>
2025-11-05 17:45:53 +0100

    2

--- src/App.java
+++ src/App.java
@@ -1,21 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.print("Por favor, introduzca la altura de la X: ");
-        int altura = Integer.parseInt(System.console().readLine());
+        try {
+            System.out.print("Por favor, introduzca la altura de la X: ");
+            int altura = Integer.parseInt(System.console().readLine());
 
-        if (altura < 3 || altura % 2 == 0) {
-            System.out.println("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
-        }else{
-            for(int fila = 0; altura >= fila; fila++){
-                for(int columna=0; columna < altura;columna++){
-                    if (columna == fila || columna == altura-fila-1) {
-                        System.out.print("*");
-                    }else{
-                        System.out.print(" ");
+            if (altura < 3 || altura % 2 == 0) {
+                System.out.println("Datos incorrectos. Debe introducir una altura impar mayor o igual a 3");
+            } else {
+                for (int fila = 0; altura >= fila; fila++) {
+                    for (int columna = 0; columna < altura; columna++) {
+                        if (columna == fila || columna == altura - fila - 1) {
+                            System.out.print("*");
+                        } else {
+                            System.out.print(" ");
+                        }
                     }
+                    System.out.println();
                 }
-                System.out.println();
             }
+        } catch (NumberFormatException e) {
+            System.out.println("Número mal puesto.");
+        } catch (Exception e) {
+            System.out.println("");
         }
     }
 }
